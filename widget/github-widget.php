6a357c3f77451036bc8694095fdf7465c96338bf
Dan Holloran <dtholloran@gmail.com>
2013-03-17 09:26:10 -0500

    Cleaned up and fixed comments

--- widget/github-widget.php
+++ widget/github-widget.php
@@ -4,97 +4,123 @@
  */
 class WP_Github_Recent_Commit_Widget extends WP_Widget {
 	private $fields = array(
-		'title'          				=> 'Title (optional)',
-		'github_username' 			=> 'Github Username',
-		'show_octocat' 					=> 'Show Random Octocat',
-		'octocat_size_width'		=>	'Octocat Width',
-		'octocat_size_height'		=>	'Octocat Height'
+		'title'              => 'Title (optional)',
+		'github_username'    => 'Github Username (required)',
+		'show_octocat'      => 'Show Random Octocat',
+		'octocat_size_width'  => 'Octocat Width (default: 100px)',
+		'octocat_size_height'  => 'Octocat Height (default: 100px)'
 	);
 
+	/**
+	 * Constructor
+	 */
 	function __construct() {
-		$widget_ops = array('classname' => 'widget_dh_github_widget', 'description' => __('Simple Github widget.', 'roots'));
+		$widget_ops = array( 'classname' => 'widget_dh_github_widget', 'description' => __( 'Simple Github widget.', 'roots' ) );
 
-		$this->WP_Widget('widget_dh_github_widget', __('Github Widget', 'roots'), $widget_ops);
+		$this->WP_Widget( 'widget_dh_github_widget', __( 'Github Widget', 'roots' ), $widget_ops );
 		$this->alt_option_name = 'widget_dh_github_widget';
 
-		add_action('save_post', array(&$this, 'flush_widget_cache'));
-		add_action('deleted_post', array(&$this, 'flush_widget_cache'));
-		add_action('switch_theme', array(&$this, 'flush_widget_cache'));
-	}
+		add_action( 'save_post', array( &$this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( &$this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( &$this, 'flush_widget_cache' ) );
+		if ( is_active_widget( false, false, $this->id_base, true ) )
+			add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_files' ) );
+	} // __construct()
 
-	function widget($args, $instance) {
-		$cache = wp_cache_get('widget_dh_github_widget', 'widget');
 
-		if (!is_array($cache)) {
-			$cache = array();
-		}
+	/**
+	 * Widget
+	 */
+	function widget( $args, $instance ) {
+		$cache = wp_cache_get( 'widget_dh_github_widget', 'widget' );
 
-		if (!isset($args['widget_id'])) {
-			$args['widget_id'] = null;
-		}
+		if ( !is_array( $cache ) ) $cache = array();
 
-		if (isset($cache[$args['widget_id']])) {
+		if ( !isset( $args['widget_id'] ) ) $args['widget_id'] = null;
+
+		if ( isset( $cache[$args['widget_id']] ) ) {
 			echo $cache[$args['widget_id']];
 			return;
 		}
 
 		ob_start();
-		extract($args, EXTR_SKIP);
+		extract( $args, EXTR_SKIP );
 
-		$title = apply_filters('widget_title', empty($instance['title']) ? __('', 'roots') : $instance['title'], $instance, $this->id_base);
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( '', 'roots' ) : $instance['title'], $instance, $this->id_base );
 
-		foreach($this->fields as $name => $label) {
-			if (!isset($instance[$name])) { $instance[$name] = ''; }
+		foreach ( $this->fields as $name => $label ) {
+			if ( !isset( $instance[$name] ) ) { $instance[$name] = ''; }
 		}
 
 		echo $before_widget;
 
-		if ($title) {
-		  echo $before_title, $title, $after_title;
-		}
+		if ( $title )
+			echo $before_title, $title, $after_title;
+
 		require_once "views/view-github-widget.php";
 		echo $after_widget;
 
 		$cache[$args['widget_id']] = ob_get_flush();
-		wp_cache_set('widget_dh_github_widget', $cache, 'widget');
-	}
+		wp_cache_set( 'widget_dh_github_widget', $cache, 'widget' );
+	} // widget()
 
-	function update($new_instance, $old_instance) {
-		$instance = array_map('strip_tags', $new_instance);
+
+	/**
+	* Update
+	*/
+	function update( $new_instance, $old_instance ) {
+		$instance = array_map( 'strip_tags', $new_instance );
 
 		$this->flush_widget_cache();
 
-		$alloptions = wp_cache_get('alloptions', 'options');
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
 
-		if (isset($alloptions['widget_dh_github_widget'])) {
-			delete_option('widget_dh_github_widget');
-		}
+		if ( isset( $alloptions['widget_dh_github_widget'] ) )
+			delete_option( 'widget_dh_github_widget' );
 
 		return $instance;
-	}
+	} // update
 
+
+	/**
+	* Flush Widget Cache
+	*/
 	function flush_widget_cache() {
-		wp_cache_delete('widget_dh_github_widget', 'widget');
-	}
 
-	function form($instance) {
-		foreach($this->fields as $name => $label) {
-			${$name} = isset($instance[$name]) ? esc_attr($instance[$name]) : '';
-		?>
+		wp_cache_delete( 'widget_dh_github_widget', 'widget' );
+	} // flush_widget_cache()
+
+
+	/**
+	* Form
+	*/
+	function form( $instance ) {
+		foreach ( $this->fields as $name => $label ) {
+			${$name} = isset( $instance[$name] ) ? esc_attr( $instance[$name] ) : ''; ?>
 		<p>
 
 			<?php if ( $name != 'show_octocat' ): ?>
-				<label for="<?php echo esc_attr($this->get_field_id($name)); ?>"><?php _e("{$label}:", 'roots'); ?></label>
-				<input class="widefat" id="<?php echo esc_attr($this->get_field_id($name)); ?>" name="<?php echo esc_attr($this->get_field_name($name)); ?>" type="text" value="<?php echo ${$name}; ?>">
+				<label for="<?php echo esc_attr( $this->get_field_id( $name ) ); ?>"><?php _e( "{$label}:", 'roots' ); ?></label>
+				<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( $name ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( $name ) ); ?>" type="text" value="<?php echo ${$name}; ?>">
 			<?php else: ?>
 
 				<?php $checked = ( ${$name} == 'on' ) ? 'checked="checked"': ''; ?>
-				<input id="<?php echo esc_attr($this->get_field_id($name)); ?>" name="<?php echo esc_attr($this->get_field_name($name)); ?>" type="checkbox" value="on"<?php echo $checked; ?>>
-				<label for="<?php echo esc_attr($this->get_field_id($name)); ?>"><?php _e("{$label}", 'roots'); ?></label>
+				<input id="<?php echo esc_attr( $this->get_field_id( $name ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( $name ) ); ?>" type="checkbox" value="on"<?php echo $checked; ?>>
+				<label for="<?php echo esc_attr( $this->get_field_id( $name ) ); ?>"><?php _e( "{$label}", 'roots' ); ?></label>
 			<?php endif ?>
 
 		</p>
 		<?php
-		}
-	}
-}
+		} // foreach
+	} // form()
+
+
+	/**
+	* Enqueue Files
+	*/
+	function enqueue_files()
+	{
+		wp_enqueue_style( 'wpgrc_plugin_css', plugins_url( 'assets/css/wpgrc-plugin.css' , dirname(__FILE__) ) );
+	} // enqueue_files()
+
+} // WP_Github_Recent_Commit_Widget
