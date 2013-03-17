234df6e7ab4d1e78ef570127660afae8caa7a868
Dan Holloran <dtholloran@gmail.com>
2013-03-16 23:52:23 -0500

    Added checks for github error message

--- widget/views/view-github-widget.php
+++ widget/views/view-github-widget.php
@@ -1,31 +1,42 @@
 <?php
+
+// Widget Instance
 extract( $args );
 extract($instance);
 $octocat_height = ( isset( $octocat_size_height ) AND is_numeric( $octocat_size_height ) ) ? floor( $octocat_size_height ) : 100;
 $octocat_width = ( isset( $octocat_size_width ) AND is_numeric( $octocat_size_width ) ) ? floor( $octocat_size_width ) : 100;
-$github_api = new DH_Github_API_v3( strtolower( $github_username ), $widget_id );
-$widget_content = $github_api->widget_content();
-extract( $widget_content );
-extract( $octocat );
-$repo_text = explode( '/', $repo_title );
-$owner_name = $repo_text[0];
-$repo_name = $repo_text[1];
-?>
 
-<!-- GITHUB LAST COMMIT WIDGET -->
-<div class="github-last-commit pull-left">
-	<div class="github-commit-octocat polaroid pull-left">
-		<img class="pull-left" src="<?php echo $octocat_image_url; ?>" alt="<?php echo $octocat_name; ?>" width="<?php echo $octocat_width; ?>" height="<?php echo $octocat_height; ?>">
-	</div>
-	<div class="pull-left github-commit-info-wrap">
-		<div class="clear github-commit-repo-title">
-			<a href="https://github.com/<?php echo $owner_name; ?>" target="_blank"><?php echo $owner_name; ?></a><span>/</span><a href="<?php echo $repo_url; ?>" target="_blank"><?php echo $repo_name; ?></a>
+// Github API
+$github_api = new WPGRC_Github_API_v3( strtolower( $github_username ), $widget_id );
+$widget_content = $github_api->widget_content();
+if ( !empty( $widget_content ) ) {
+	extract( $widget_content );
+	extract( $octocat );
+	$repo_text = explode( '/', $repo_title );
+	$owner_name = $repo_text[0];
+	$repo_name = $repo_text[1];
+	?>
+
+	<!-- WPGRC WIDGET -->
+	<div class="wpgrc-last-commit pull-left">
+		<div class="wpgrc-commit-octocat polaroid pull-left">
+			<img class="pull-left" src="<?php echo $octocat_image_url; ?>" alt="<?php echo $octocat_name; ?>" width="<?php echo $octocat_width; ?>" height="<?php echo $octocat_height; ?>">
 		</div>
-		<div class="clear github-commit-message pull-left"><?php echo $message; ?></div>
-		<div class="clear github-commit-author-wrap pull-left">
-			<span class="pull-left">Commited by:&nbsp;</span>
-			<a href="<?php echo $author_url; ?>" class="pull-left github-commit-author" target="_blank"><?php echo $author; ?></a>
-			<div class="github-commit-avatar pull-left"></div>
+		<div class="pull-left wpgrc-commit-info-wrap">
+			<div class="clear wpgrc-commit-repo-title">
+				<a href="https://wpgrc.com/<?php echo $owner_name; ?>" target="_blank"><?php echo $owner_name; ?></a><span>/</span><a href="<?php echo $repo_url; ?>" target="_blank"><?php echo $repo_name; ?></a>
+			</div>
+			<div class="clear wpgrc-commit-message pull-left"><?php echo $message; ?></div>
+			<div class="clear wpgrc-commit-author-wrap pull-left">
+				<span class="pull-left">Commited by:&nbsp;</span>
+				<a href="<?php echo $author_url; ?>" class="pull-left wpgrc-commit-author" target="_blank"><?php echo $author; ?></a>
+				<div class="wpgrc-commit-avatar pull-left"></div>
+			</div>
 		</div>
 	</div>
-</div><!-- END LAST COMMIT WIDGET -->
+	<!-- END WPGRC WIDGET -->
+<?php } else { ?>
+	<!-- WPGRC WIDGET ERROR -->
+	<div class="wpgrc-error">I am sorry, something went wrong when contacting GitHub please try again later.</div>
+	<!-- END WPGRC WIDGET ERROR -->
+<?php } // if/else(!empty($widget_content))
