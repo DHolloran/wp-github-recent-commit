01be6ab1ec5d556beeab2e7d03e3f4f29f8d52c5
Dan Holloran <dtholloran@gmail.com>
2013-03-24 11:46:00 -0500

    1.1.0 Release

--- wp-github-recent-commit.php
+++ wp-github-recent-commit.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package WP_Github_Recent_Commit
- * @version 1.0.0
+ * @version 1.1.0
  */
 /*
 Plugin Name: WP Github Recent commit
 Plugin URI: http://dholloran.github.com/wp-github-recent-commit/
 Description: Wordpress widget that grabs a random Octocat from the Octodex and the latest commit from a public GitHub repository.
 Author: Dan Holloran
-Version: 1.0.0
+Version: 1.1.0
 Author URI: http://danholloran.com/
 */
 
